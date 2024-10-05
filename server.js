e13fffca3374a7d0cd49efbde33083513b922191
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-10-05 09:53:43 -0500

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import connectDatabase from './config/db.js';
+import User from './models/Users.js';
 
 const app = express();
 
-app.get('/',(req,res)=>
-res.send('http get request sent to root api endpoint')
-);
+app.use(express.json()); 
 
-app.listen(3000,()=>console.log(`Express server running on port 3000`));
+connectDatabase();
+
+app.get('/', (req, res) => {
+    res.send('HTTP GET request sent to root API endpoint');
+});
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body); 
+});
+
+app.listen(3000, () => console.log(`Express server running on port 3000`));
