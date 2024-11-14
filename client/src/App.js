094fb92aa88217ce2704f86bae056fef8517c9b9
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-11-13 22:22:45 -0600

    fixed port conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import './App.css';
+import axios from 'axios'
 
 class App extends React.Component{
-  render() {
-    return(
-      <div className='App'>
-        <header className='App-header'>
-          GoodThings
-        </header>
-      </div>
-    )
-  }
+state={
+  data:null,
+}
+componentDidMount() {
+  axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data,
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+}
+
+render(){
+  return(
+    <div className='App'>
+      <header className='App-Header'>
+        GoodThings
+      </header>
+      {this.state.data}
+    </div>
+  )
+}
 }
 
 export default App;
