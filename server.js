b74931eb2572db3f10d16d0e175ab590e653d3d5
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-11-25 14:22:44 -0600

    complete

--- server.js
+++ server.js
@@ -1,37 +1,83 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import connectDatabase from './config/db.js';
-import User from './models/Users.js';
+import User from './models/User.js';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import { check, validationResult } from 'express-validator';
+import config from 'config';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
-//different from instructions, I was trying to overcome cors errors,
-// turned out airplay from my mac was using port 5000
+// To overcome CORS errors
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-  }));
-  
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 
-  app.use(express.json({ extended: false }));
+app.use(express.json());
 connectDatabase();
 
 app.get('/', (req, res) => {
     res.send('HTTP GET request sent to root API endpoint');
 });
 
-app.post('/api/users', async (req, res) => {
-    try {
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
         const { name, email, password } = req.body;
-        const newUser = new User({ name, email, password });
-        await newUser.save();
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(500).json({ message: 'Error registering user', error });
+
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+
+            user = new User({
+                name,
+                email,
+                password,
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload={
+                user:{
+                    id:user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn:'10hr'},
+                (err,token)=>{
+                    if(err) throw err;
+                    res.json({token:token});
+                }
+            );
+
+        } catch (error) {
+            res.status(500).send('Server error');
+        }
+      
     }
-});
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
